23a43850b27f1504977bc762664953485aa4bdf0
Ramon Perdomo <rperdomorivas@gmail.com>
2022-06-18 15:58:39 -0400

    import styles in root.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Asteroids from "pages/Asteroids";
 
+import "styles/main.css";
+
 function App() {
   return (
     <Router>
