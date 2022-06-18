3099127f0381c71cd108b52c3ddb0767b32e0606
Ramon Perdomo <rperdomorivas@gmail.com>
2022-06-18 15:48:01 -0400

    create page Asteroids.

---
+++ src/pages/Asteroids.jsx
@@ -0,0 +1,13 @@
+import Header from "components/Header";
+
+function Asteroids() {
+  return (
+    <>
+      <Header />
+
+      <span>Here go some fancy asteroids data</span>
+    </>
+  );
+}
+
+export default Asteroids;
