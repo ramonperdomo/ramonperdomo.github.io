470a63a57a2d86405f3e5823d387f475751970e0
Ramon Perdomo <rperdomorivas@gmail.com>
2022-06-19 22:35:27 -0400

    register constants for responsive breakpoints.

---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+/** RESPONSIVE CONSTANTS */
+// screen min-width - for responsive breakpoints
+export const PORTRAIT_TABLETS_AND_LARGE_PHONES_WIDTH = 576;
+export const LANDSCAPE_TABLETS_WIDTH = 768;
+export const LAPTOPS_DESKTOPS_WIDTH = 992;
+export const LARGE_LAPTOPS_AND_DESKTOPS_WIDTH = 1200;
+export const EXTRA_LARGE_LAPTOPS_AND_DESKTOPS_WIDTH = 1200;
+
+// screen width level - for responsive breakpoints
+export const PHONES = 1;
+export const PORTRAIT_TABLETS_AND_LARGE_PHONES = 2;
+export const LANDSCAPE_TABLETS = 3;
+export const LAPTOPS_DESKTOPS = 4;
+export const LARGE_LAPTOPS_AND_DESKTOPS = 5;
+export const EXTRA_LARGE_LAPTOPS_AND_DESKTOPS = 6;
