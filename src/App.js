eb405f5e25a8671b5e09ee796e450c031854e5ec
Ramon Perdomo <rperdomorivas@gmail.com>
2022-06-19 22:35:36 -0400

    make reactive change of screen width.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
+import React from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Asteroids from "pages/Asteroids";
 
 import "styles/main.css";
 
+export const WidthContext = React.createContext(null);
+
 function App() {
+  const [width, setWidth] = useState(getWidthLevel(window.innerWidth));
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(getWidthLevel(window.innerWidth));
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Asteroids />}></Route>
-      </Routes>
-    </Router>
+    <WidthContext.Provider value={width}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Asteroids />}></Route>
+        </Routes>
+      </Router>
+    </WidthContext.Provider>
   );
 }
+
+// for more info read utils/constants.js, section RESPONSIVE CONSTANTS
+const getWidthLevel = (/** @type {number} */ width) => {
+  if (width < 576) return 1; // phones
+  else if (width < 768) return 2; // portrait tablets and large phones
+  else if (width < 992) return 3; // landscape tablets
+  else if (width < 1200) return 4; // laptops/desktops
+  else if (width < 1400) return 5; // large laptops and desktops
+  else return 6; // extra large laptops and desktops
+};
 
 export default App;
